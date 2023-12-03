2929dde4a0f3101b14a58fa6dc977422379676fb
Heshan <mhsandamal1@gmail.com>
2023-12-03 10:37:57 +0000

    Tidy up

--- dec3/puzzle-2.py
+++ dec3/puzzle-2.py
@@ -9,6 +9,7 @@ print(matrix)
 
 
 # Get all adjacent cell to *
+# Return [(x,y),(x,y)]
 def get_adjacent_cells_indexes(row, col):
     rows, cols = len(matrix), len(matrix[0])
 
@@ -26,18 +27,18 @@ def is_adj_number_exists(x, y):
 
 
 # Create number using left numbers + given cell + right numbers
-def get_number(cell):
-    number = matrix[cell[0]][cell[1]]
-    x = cell[0]
+def get_number(cell_indexes):
+    number = matrix[cell_indexes[0]][cell_indexes[1]]
+    x = cell_indexes[0]
 
     # Add Left side of numbers
-    i = cell[1] - 1
+    i = cell_indexes[1] - 1
     while i >= 0 and matrix[x][i].isnumeric():
         number = str(matrix[x][i]) + str(number)
         i = i - 1
 
     # Add Right side of numbers
-    j = cell[1] + 1
+    j = cell_indexes[1] + 1
     while j < len(matrix[x]) and matrix[x][j].isnumeric():
         number = str(number) + str(matrix[x][j])
         j = j + 1
@@ -51,13 +52,13 @@ for x in range(len(matrix)):
     row = []
     for y in range(len(matrix[x])):
         if matrix[x][y] == "*":
-            indexes = get_adjacent_cells_indexes(x, y)
+            all_adjacent_cell_indexes = get_adjacent_cells_indexes(x, y)
 
-            if len(indexes) >= 2:
+            if len(all_adjacent_cell_indexes) >= 2:
                 prev = {"number": -1, "start_index": -1, "end_index": -1, "row": -1}
                 number_array = []
-                for t in indexes:
-                    data = get_number(t)
+                for cellIndexes in all_adjacent_cell_indexes:
+                    data = get_number(cellIndexes)
                     # Avoid adding the same number in the same row
                     if data["row"] == prev["row"]:
                         if prev["start_index"] != data["start_index"] and prev["end_index"] != data["end_index"]:
@@ -66,8 +67,10 @@ for x in range(len(matrix)):
                     else:
                         number_array.append(data["number"])
                         prev = data
+
                 if len(number_array) == 2:
                     total += (number_array[0] * number_array[1])
                     row.append(number_array)
+
     print("row", x, row)
 print("Total", total)
