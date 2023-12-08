b2dbd5764b7604ed6ac723dca44240d3f48e8b3e
Heshan <mhsandamal1@gmail.com>
2023-12-08 20:25:20 +0000

    tidy up

--- dec8/puzzle-2-answer.py
+++ dec8/puzzle-2-answer.py
@@ -1,6 +1,8 @@
 """After waiting for sometime for the answer from the brute force approach I did a small analysis on why its not giving the answer
 Problem is, when element A element find Z element, then after some time it again comes to the same Z element & cycle continues
 
+Used puzzle-1 answer to calculate the cycle sizes for each A node.
+
 start , end , cycle-size
 
 PBA -> RGZ -> 20093 -> RGZ -> 20093
@@ -17,5 +19,4 @@ All this will be in the particular Z element when the count equals the least com
 """
 
 import math
-
 print(math.lcm(20093, 12169, 13301, 20659, 16697, 17263))
