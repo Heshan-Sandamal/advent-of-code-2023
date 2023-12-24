5f0a5b88de33762463e97e91c521e530fa72daa3
Heshan <mhsandamal1@gmail.com>
2023-12-24 15:08:09 +0000

    Dec 24- Puzzle 1

--- dec-24/puzzle-1.py
+++ dec-24/puzzle-1.py
@@ -1,50 +1,50 @@
 import sys
-
 import numpy
 import numpy as np
+
+min_size, max_size = 200000000000000, 400000000000000
 
 with open("input.txt") as file:
     lines = file.read().splitlines()
 
-given_size = (200000000000000, 400000000000000)
 
-
+# Here this formula is derived by formulating the formula for each line
+# x = x1 + v1x*t , y = y1 + v1y*t
+# Then remove t from above 2 and create a formula for the line something like y = a*x + b using x0,v1,y0,v2
+# Get the line formulas for a pair
+# And then make them equal and find the x and y values of the intersection
 def calculate(x1, y1, v1x, v1y, x2, y2, v2x, v2y):
     try:
         x = ((-v1x * v2x * y1 + (v1y * v2x * x1) + v1x * v2x * y2 - v1x * v2y * x2)) / (v1y * v2x - v1x * v2y)
         y = (v1y / v1x) * x + (y1 - (v1y * x1) / v1x)
-        # y2 = (v2y * x) / v2x + (y2 - v2y * x2 / v2x)
         return x, y
     except ZeroDivisionError:
         return -sys.maxsize, -sys.maxsize
-
-
-def equation_of_line(x0, y0, vx, vy):
-    slope = vy / vx
-    intercept = y0 - slope * x0
-    return f"y = {slope} * x + {intercept}"
 
 
 total = 0
 data = []
 for line in lines:
     position, velocity = line.split("@")
-    position_vector = np.array(list(map(int, position.split(","))),dtype=numpy.float64)
-    velocity_vector = np.array(list(map(int, velocity.split(","))),dtype=numpy.float64)
-
+    position_vector = np.array(list(map(int, position.split(","))), dtype=numpy.float64)
+    velocity_vector = np.array(list(map(int, velocity.split(","))), dtype=numpy.float64)
     data.append(({"p": position_vector, "v": velocity_vector}))
-
-print(data)
 
 intersections = 0
 for i, cell in enumerate(data):
     x1, y1, v1x, v1y = cell["p"][0], cell["p"][1], cell["v"][0], cell["v"][1]
-    other_cells = data[i + 1:]
-    for other_cell in other_cells:
+    for other_cell in data[i + 1:]:
         x2, y2, v2x, v2y = other_cell["p"][0], other_cell["p"][1], other_cell["v"][0], other_cell["v"][1]
+
+        # Get the intersection point
         x, y = calculate(x1, y1, v1x, v1y, x2, y2, v2x, v2y)
 
-        if (given_size[0] <= x <= given_size[1] and given_size[0] <= y <= given_size[1]):
+        # Check whether the intersection point falls within the given area
+        if (min_size <= x <= max_size and min_size <= y <= max_size):
+
+            # Check whether the point is a forward one by checking new coordinate is in the direction of the velocity
+            # Eg: (x - x1) * v1x >0 means point new point's x coordinate is in the v1x direction
+            # hence its a forward not a collision in the past
             if ((x - x1) * v1x >= 0 and (y - y1) * v1y >= 0 and (x - x2) * v2x > 0 and (y - y2) * v2y >= 0):
                 intersections += 1
 
