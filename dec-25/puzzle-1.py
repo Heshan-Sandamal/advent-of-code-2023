c1c850ddffd98b8cf0cd5eaeee0800bea916435e
Heshan <mhsandamal1@gmail.com>
2023-12-25 09:33:12 +0000

    Dec-25

---
+++ dec-25/puzzle-1.py
@@ -0,0 +1,29 @@
+from networkx import connected_components
+import networkx as nx
+
+with open("input.txt") as file:
+    lines = file.read().splitlines()
+
+# Create undirected Graph
+G = nx.Graph()
+for line in lines:
+    node, other_nodes = line.split(":")[0].strip(), line.split(":")[1].strip().split(" ")
+    G.add_node(node)
+    for x in other_nodes:
+        G.add_node(x)
+        G.add_edge(node, x)
+
+print(G)
+
+# Dividing the graph into two means its a cut and this provides the minimum cut of the graph
+# Which is the one given as the requirement
+cuts = list(nx.minimum_edge_cut(G))
+print(cuts)
+
+G.remove_edge(cuts[0][0], cuts[0][1])
+G.remove_edge(cuts[1][0], cuts[1][1])
+G.remove_edge(cuts[2][0], cuts[2][1])
+
+# This calculates the sizes of two disjoint sets once graph is divided by the cut
+sub_graph = list(connected_components(G))
+print(len(sub_graph[0]) * len(sub_graph[1]))
---
+++ dec-25/puzzle-2.py
@@ -0,0 +1,1 @@
+# This is not enabled for me yet, since I need to get 3 more stars to restart the system :); :); :); :); :);
