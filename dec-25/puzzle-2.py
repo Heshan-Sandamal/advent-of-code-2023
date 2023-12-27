a7ea661ca5dc8b46749e366eef786da45025cb8c
Heshan <mhsandamal1@gmail.com>
2023-12-27 13:26:22 +0000

    Dec 25, Puzzle 2

--- dec-25/puzzle-2.py
+++ dec-25/puzzle-2.py
@@ -1,1 +1,3 @@
-# This is not enabled for me yet, since I need to get 3 more stars to restart the system :); :); :); :); :);
+# Just push the big red button and puzzle is done :) :) :)
+# Most difficult one to solve since I had 2 other remaining puzzles to solve to push the button :) :)
+# Finally it's done & it's the end of AOC 2023 adventure which is my first AOC, and I really enjoyed the journey
